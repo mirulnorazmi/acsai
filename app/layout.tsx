3429f3a4fbdb4a11e079e3751a2229f7fc25c7ce
mirulnorazmi <mmiirul00@gmail.com>
2026-02-07 16:19:03 +0800

    test deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" title="test">
       <body className="antialiased">
         <Providers>{children}</Providers>
       </body>
